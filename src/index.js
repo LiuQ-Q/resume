9eb69ce2d0b7caf611b970f28c148f4d02282151
LiuQ-Q <545784977@qq.com>
2019-10-13 19:51:22 +0800

    [FIXED] bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import BootstrapVue from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
 
 const app = new Vue({
   router,
