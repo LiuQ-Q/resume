507125eb5ad085dfb126b7ec2d75b02522a88f77
LiuQ-Q <545784977@qq.com>
2019-10-14 23:07:02 +0800

    [FIXED] swiper

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'swiper/dist/css/swiper.css'
 
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import BootstrapVue from 'bootstrap-vue'
+import BootstrapVue from 'bootstrap-vue';
+import VueAwesomeSwiper from 'vue-awesome-swiper';
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
+Vue.use(VueAwesomeSwiper);
 
 const app = new Vue({
   router,
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <b-navbar toggleable="lg" type="dark" variant="info">
+  <div style="height:100%;">
+    <!-- <b-navbar toggleable="lg" type="dark" variant="info">
       <b-navbar-brand href="#">NavBar</b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
@@ -11,18 +11,39 @@
           <b-nav-item href="#" disabled>Disabled</b-nav-item>
         </b-navbar-nav>
 
-        <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
 
         </b-navbar-nav>
       </b-collapse>
-    </b-navbar>
+    </b-navbar> -->
+
+    <swiper  style="height:100%;" :options="swiperOption">
+      <swiper-slide style="background:red;">Slide 1</swiper-slide>
+      <swiper-slide style="background:blue;">Slide 2</swiper-slide>
+      <swiper-slide style="background:green;">Slide 3</swiper-slide>
+      <swiper-slide style="background:gray;">Slide 4</swiper-slide>
+      <swiper-slide style="background:black;">Slide 5</swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      swiperOption: {
+        direction: 'vertical',
+        slidesPerView: 1,
+        spaceBetween: 0,
+        mousewheel: true,
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        }
+      }
+    }
+  }
 }
 </script>
 
