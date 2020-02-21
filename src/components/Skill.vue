bd05acf0506092d4efc39a639430291c2efda845
LiuQ-Q <545784977@qq.com>
2020-02-21 20:40:19 +0800

    [ADDED] Skill

---
+++ src/components/Skill.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="skill">
+    <!-- <b-container> -->
+      <h2>技能</h2>
+      <div class="content-skill">
+        <ul class="skill-in">
+          <li
+            v-for="(skill, index) in skillIn" :key="index"
+          >{{ skill }}</li>
+        </ul>
+        <ul class="skill-out">
+          <li
+            v-for="(skill, index) in skillOut" :key="index"
+          >{{ skill }}</li>
+        </ul>
+      </div>
+
+      <div class="content-info">
+        <p>熟悉css+div布局，熟悉JavaScript，了解HTML语义化</p>
+        <p>熟悉浏览器的兼容，前端性能优化</p>
+        <p>熟悉HTML5，熟悉响应式布局，百分比自适应布局</p>
+        <p>熟悉CSS3动画技术，Animate.css动画库</p>
+        <p>熟悉AJAX，JSON，熟悉正则表达式，JSONP跨域请求</p>
+        <p>熟悉jQuery框架的使用，了解jQuery的DOM操作及事件处理</p>
+        <p>熟悉node.js，熟悉vue框架的使用</p>
+        <p>熟悉Sublime，WebStorm，Fireworks，Photoshop等开发工具</p>
+      </div>
+    <!-- </b-container> -->
+  </div>
+</template>
+
+<script>
+import './skill.scss';
+
+export default {
+ data() {
+   return {
+     skillIn: ['HTML', 'CSS', 'JS'],
+     skillOut: ['jQuery', 'Ajax', 'JSON', 'PS', 'HTML5', 'CSS3', 'Vue']
+   }
+ },
+}
+</script>
+
+<style>
+
+</style>
--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -8,10 +8,15 @@
           <Home></Home>
         </div>
       </swiper-slide>
+
       <swiper-slide class="slide-introduce slide-top">
         <Introduce></Introduce>
       </swiper-slide>
-      <swiper-slide style="background:green;">Slide 3</swiper-slide>
+
+      <swiper-slide class="slide-skill slide-top">
+        <Skill></Skill>
+      </swiper-slide>
+
       <swiper-slide style="background:gray;">Slide 4</swiper-slide>
       <swiper-slide style="background:black;">Slide 5</swiper-slide>
       <div class="swiper-pagination" slot="pagination"></div>
@@ -23,12 +28,14 @@
 import myHeader from '../components/Header.vue';
 import Home from '../components/Home.vue';
 import Introduce from '../components/Introduce.vue';
+import Skill from '../components/Skill.vue';
 
 export default {
   components: {
     myHeader,
     Home,
-    Introduce
+    Introduce,
+    Skill
   },
   data() {
     return {
@@ -63,5 +70,8 @@ export default {
 }
 .slide-introduce {
   background: url('../assets/cover.png') #107bb3;
+}
+.slide-skill {
+  background: url('../assets/cover.png') #DEDEDE;
 }
 </style>
